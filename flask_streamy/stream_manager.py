76c0d4f1bae4294edc6aef3d3b68edb00fab0677
Nicholas Mendez <snickdx@gmail.com>
2024-08-26 01:33:13 +0000

    version 0.1.2 custom headers, logging, keep alive

--- flask_streamy/stream_manager.py
+++ flask_streamy/stream_manager.py
@@ -1,25 +1,41 @@
 from flask import Response
 from .sse import SSE
+import logging
 
 class StreamManager:
     def __init__(self):
         self.streams = {}
+        logging.basicConfig(level=logging.INFO)
 
     def send_event(self, stream_id, data, event_name=None):
         if stream_id in self.streams:
-            self.streams[stream_id].add_message(data, event_name)
+            try:
+                self.streams[stream_id].add_message(data, event_name)
+            except Exception as e:
+                logging.error(f"Failed to send event to stream {stream_id}: {e}")
         else:
-            raise ValueError(f"Stream {stream_id} does not exist.")
+            logging.error(f"Stream {stream_id} does not exist.")
 
-    def get_stream(self, stream_id, event_name="message"):
+    def get_stream(self, stream_id, event_name="message", headers=None, keep_alive_interval=30):
         if stream_id not in self.streams:
-            self.streams[stream_id] = SSE(stream_id, event_name)
+            self.streams[stream_id] = SSE(stream_id, event_name, keep_alive_interval)
+            logging.info(f"Created new stream with ID {stream_id}.")
         sse_instance = self.streams[stream_id]
-        return Response(sse_instance.stream(), content_type='text/event-stream')
+        response = Response(sse_instance.stream(), content_type='text/event-stream')
+        
+        # Add default keep-alive header
+        response.headers["Connection"] = "keep-alive"
+        
+        # Add custom headers
+        if headers:
+            for key, value in headers.items():
+                response.headers[key] = value
+                
+        return response
 
     def end_stream(self, stream_id):
         if stream_id in self.streams:
             self.streams[stream_id].end_stream()
             del self.streams[stream_id]
         else:
-            raise ValueError(f"Stream {stream_id} does not exist.")
+            logging.error(f"Stream {stream_id} does not exist.")
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="flask-streamy",
-    version="0.1.0",
+    version="0.1.2",
     description="A Flask package for managing SSE streams.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Your Name",
-    author_email="nicholas@mendez.codes",
+    author="Nicholas Mendez",
+    author_email="snickdx@gmail.com",
     url="https://github.com/snickdx/flask-streamy",
     packages=find_packages(),
     include_package_data=True,
